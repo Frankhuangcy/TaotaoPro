aa7e4bd0022f61b95f7967e58fe1c8c5f465e77e
hcy <768678360@qq.com>
2018-08-07 15:11:48 +0800

    逆向工程生成 mapper,接口以及 bean类

---
+++ TaotaoProject/src/main/java/com/hcy/bean/Admin.java
@@ -0,0 +1,63 @@
+package com.hcy.bean;
+
+public class Admin {
+    private Integer adminId;
+
+    private String loginname;
+
+    private String password;
+
+    private String name;
+
+    private Long telephone;
+
+    private String email;
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getLoginname() {
+        return loginname;
+    }
+
+    public void setLoginname(String loginname) {
+        this.loginname = loginname == null ? null : loginname.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Long getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Long telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+}
